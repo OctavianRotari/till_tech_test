1f3a8952cd33f6b2d869736d2d9580f42a573f95
octavian rotari <octavianrotari@octavians-MacBook-Pro.local>
2016-01-14 14:01:11 +0000

    it calculates the total on items and the tax

---
+++ spec/HipsterTillSpec.js
@@ -0,0 +1,97 @@
+describe("HipsterTill", function() {
+  var hipsterTill;
+  var orderSpec = [{"item": "Choc Mousse", "price": 8.20, "quantity" : 1}];
+  var orderDoubleSpec = [{"item": "Choc Mousse", "price": 8.20, "quantity" : 2}];
+  var menuSpec = {
+    "shopName": "The Coffee Connection",
+    "address": "123 Lakeside Way",
+    "phone": "16503600708",
+    "prices": [
+      {
+        "Cafe Latte": 4.75,
+        "Flat White": 4.75,
+        "Cappucino": 3.85,
+        "Single Espresso": 2.05,
+        "Double Espresso": 3.75,
+        "Americano": 3.75,
+        "Cortado": 4.55,
+        "Tea": 3.65,
+        "Choc Mudcake": 6.40,
+        "Choc Mousse": 8.20,
+        "Affogato": 14.80,
+        "Tiramisu": 11.40,
+        "Blueberry Muffin": 4.05,
+        "Chocolate Chip Muffin": 4.05,
+        "Muffin Of The Day": 4.55
+      }
+    ]
+  };
+
+  beforeEach(function() {
+     hipsterTill = new HipsterTill();
+  });
+
+  it("it prints the menu of the coffe shop", function() {
+    expect(hipsterTill.printMenu(menu)).toEqual(menuSpec);
+  });
+
+  it("it prints out the items and the prices", function() {
+    expect(hipsterTill.printPrices()).toEqual(menuSpec.prices[0]);
+  });
+
+  describe("when an order is placed", function() {
+
+    it("it ads the ordered item to the order", function() {
+      hipsterTill.addToOrder("Choc Mousse");
+      expect(hipsterTill.order).toEqual(orderSpec);
+    });
+
+    it("it overites the default value", function() {
+      hipsterTill.addToOrder("Choc Mousse", 3);
+      expect(hipsterTill.order[0].quantity).toEqual(3);
+    });
+
+    it("if is the same item it increases the quantity", function() {
+      hipsterTill.addToOrder("Choc Mousse");
+      hipsterTill.addToOrder("Choc Mousse");
+      expect(hipsterTill.order).toEqual(orderDoubleSpec);
+    });
+
+  });
+  
+  describe('when the total is calculated', function(){
+    
+    it("calculates the total of the order", function(){
+      hipsterTill.addToOrder("Choc Mousse");
+      hipsterTill.addToOrder("Choc Mousse");
+      expect(hipsterTill.calcTotal()).toEqual((16.40).toFixed(2))
+    });
+
+    it("calculates the tax of the order", function(){
+      hipsterTill.addToOrder("Choc Mousse");
+      hipsterTill.addToOrder("Choc Mousse");
+      expect(hipsterTill.calcTax()).toEqual('1.42')
+    })
+  })
+//
+//  // demonstrates use of spies to intercept and test method calls
+//  it("tells the current song if the user has made it a favorite", function() {
+//    spyon(song, 'persistfavoritestatus');
+//
+//    player.play(song);
+//    player.makefavorite();
+//
+//    expect(song.persistfavoritestatus).tohavebeencalledwith(true);
+//  });
+//
+//  //demonstrates use of expected exceptions
+//  describe("#resume", function() {
+//    it("should throw an exception if song is already playing", function() {
+//      player.play(song);
+//
+//      expect(function() {
+//        player.resume();
+//      }).tothrowerror("song is already playing");
+//    });
+//  });
+});
---
+++ src/HipsterTill.js
@@ -0,0 +1,64 @@
+function HipsterTill() {
+  this.myMenu = {},
+  this.prices = {},
+  this.order = []
+  this.newItem = true,
+  this.total = 0,
+  this.TAX = 8.64/100,
+  this.taxOnTotal = 0
+}
+
+HipsterTill.prototype.printMenu = function(menu){
+  this.myMenu = menu;
+  return this.myMenu;
+}
+
+HipsterTill.prototype.printPrices = function(){
+  this.printMenu(menu);
+  this.prices = this.myMenu.prices[0];
+  return this.prices;
+}
+
+HipsterTill.prototype.addToOrder = function(order, quantity){
+  if ( typeof(quantity) === 'undefined') quantity = 1;
+  this.printPrices();
+  this.similarOrder(order, quantity);
+  if ( this.newItem ) {
+    this.newOrder(order, quantity);
+  };
+}
+
+HipsterTill.prototype.similarOrder = function(order, quantity){
+  for(var i = 0; i < this.order.length; i++ ){
+    if(this.order[i].item === order ){
+      this.order[0].quantity += quantity;
+      this.newItem = false;
+    }
+  }
+}
+
+HipsterTill.prototype.newOrder = function(order, quantity){
+  for(i in this.prices){
+    if(i === order){
+      var obj = {};
+      obj["item"] = order;
+      obj["price"] = this.prices[order];
+      obj["quantity"] = quantity;
+      this.order.push(obj);
+    }
+  }
+}
+
+HipsterTill.prototype.calcTotal = function(){
+  for(var i=0; i < this.order.length; i++){
+    var singleItemTot = this.order[i].price * this.order[i].quantity;
+    this.total += singleItemTot;
+    return this.total.toFixed(2);
+  } 
+}
+
+HipsterTill.prototype.calcTax = function(){
+  this.calcTotal();
+  this.taxOnTotal = this.total * this.TAX; 
+  return this.taxOnTotal.toFixed(2);
+}
